d67371d896e87285523881f46436bc1151f92c24
Anh Phuong <trananhphuong1187@gmail.com>
2023-04-13 23:14:03 +0700

    handle product rating

---
+++ ShopeeClone/src/components/ProductRating/ProductRating.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function ProductRating({ rating }: { rating: number }) {
+  const handleWidthStar = (order: number) => {
+    if (order <= rating) {
+      return '100%'
+    } else if (order > rating && order - rating < 1) {
+      return rating - Math.floor(rating) * 100 + '%'
+    } else {
+      return '0%'
+    }
+  }
+  return (
+    <div className='flex items-center'>
+      {Array(5)
+        .fill(0)
+        .map((_, index) => (
+          <div className='relative' key={index}>
+            <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: handleWidthStar(index + 1) }}>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='currentColor'
+                className='h-3 w-3 fill-yellow-300 text-yellow-300'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+                  clipRule='evenodd'
+                />
+              </svg>
+            </div>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='h-3 w-3 fill-current text-gray-300'
+            >
+              <path
+                fillRule='evenodd'
+                d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+                clipRule='evenodd'
+              />
+            </svg>
+          </div>
+        ))}
+    </div>
+  )
+}
--- ShopeeClone/src/pages/ProductList/ProductItem/ProductItem.tsx
+++ ShopeeClone/src/pages/ProductList/ProductItem/ProductItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ProductRating from 'src/components/ProductRating'
 import { Product as ProductType } from 'src/types/product.type'
 import { formatCurency, formatShortNumber } from 'src/utils/utils'
 
@@ -27,36 +28,7 @@ export default function ProductItem({ product }: Props) {
             </div>
           </div>
           <div className='mt-3 flex items-center justify-between'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 24 24'
-                    fill='currentColor'
-                    className='h-3 w-3 fill-yellow-300 text-yellow-300'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-                </div>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  fill='currentColor'
-                  className='h-3 w-3 fill-current text-gray-300'
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
-                    clipRule='evenodd'
-                  />
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={product.rating} />
             <div className='ml-2 text-sm'>
               <span>{formatShortNumber(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>

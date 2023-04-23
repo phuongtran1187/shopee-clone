5c94eff54cbc258ccb546abaad38e52b56cd9d01
Anh Phuong <trananhphuong1187@gmail.com>
2023-04-23 13:22:41 +0700

    handle rating filter

--- ShopeeClone/src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ ShopeeClone/src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -10,6 +10,8 @@ import { Controller, useForm } from 'react-hook-form'
 import { SchemaType, schema } from 'src/utils/rulesValidation'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { NoUndefined } from 'src/types/utils.type'
+import RatingStar from '../RatingStar'
+import { omit } from 'lodash'
 
 interface Props {
   categories: Category[]
@@ -26,7 +28,6 @@ export default function AsideFilter({ categories, queryConfig }: Props) {
   const {
     control,
     handleSubmit,
-    watch,
     formState: { errors }
   } = useForm<FormData>({
     defaultValues: {
@@ -47,6 +48,16 @@ export default function AsideFilter({ categories, queryConfig }: Props) {
       }).toString()
     })
   })
+
+  const handleRemoveAllFilter = () => {
+    navigate({
+      pathname: path.home,
+      search: createSearchParams(
+        omit({ ...queryConfig }, ['price_max', 'price_min', 'category', 'rating_filter'])
+      ).toString()
+    })
+  }
+
   return (
     <div className='py-4'>
       <Link
@@ -182,54 +193,12 @@ export default function AsideFilter({ categories, queryConfig }: Props) {
       </div>
       <div className='my-4 h-[1px] bg-gray-300' />
       <div className='text-sm'>Đánh giá</div>
-      <ul className='my-3'>
-        <li className='py-1 pl-2'>
-          <Link to='' className='flex items-center text-sm'>
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  fill='currentColor'
-                  className='mr-1 h-4 w-4'
-                  key={index}
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
-                    clipRule='evenodd'
-                  />
-                </svg>
-              ))}
-            Trở lên
-          </Link>
-        </li>
-        <li className='py-1 pl-2'>
-          <Link to='' className='flex items-center text-sm'>
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  fill='currentColor'
-                  className='mr-1 h-4 w-4'
-                  key={index}
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
-                    clipRule='evenodd'
-                  />
-                </svg>
-              ))}
-            Trở lên
-          </Link>
-        </li>
-      </ul>
+      <RatingStar queryConfig={queryConfig} />
       <div className='my-4 h-[1px] bg-gray-300' />
-      <Button className='mt-2 flex w-full items-center justify-center bg-orange p-2 text-sm uppercase text-white hover:bg-orange/80'>
+      <Button
+        className='mt-2 flex w-full items-center justify-center bg-orange p-2 text-sm uppercase text-white hover:bg-orange/80'
+        onClick={handleRemoveAllFilter}
+      >
         Xoá tất cả
       </Button>
     </div>
---
+++ ShopeeClone/src/pages/ProductList/RatingStar/RatingStar.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { QueryConfig } from '../ProductList'
+import { createSearchParams, useNavigate } from 'react-router-dom'
+import { path } from 'src/constants/path'
+
+interface Props {
+  queryConfig: QueryConfig
+}
+
+export default function RatingStar({ queryConfig }: Props) {
+  const navigate = useNavigate()
+
+  const handleFilterByRatingStar = (rating_star: number) => {
+    navigate({
+      pathname: path.home,
+      search: createSearchParams({
+        ...queryConfig,
+        rating_filter: String(5 - rating_star)
+      }).toString()
+    })
+  }
+
+  return (
+    <ul className='my-3'>
+      {Array(5)
+        .fill(0)
+        .map((_, index) => (
+          <li className='py-1 pl-2' key={index}>
+            <div
+              className='flex items-center text-sm'
+              onClick={() => handleFilterByRatingStar(index)}
+              tabIndex={0}
+              role='button'
+              aria-hidden='true'
+            >
+              {Array(5)
+                .fill(0)
+                .map((_, indexStar) => {
+                  if (indexStar < 5 - index) {
+                    return (
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='mr-1 h-4 w-4 text-yellow-400'
+                        key={indexStar}
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                    )
+                  }
+                  return (
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 24 24'
+                      fill='none'
+                      stroke='currentColor'
+                      className='mr-1 h-4 w-4 text-yellow-400'
+                      key={indexStar}
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                  )
+                })}
+              {index !== 0 && <span>Trở lên</span>}
+            </div>
+          </li>
+        ))}
+    </ul>
+  )
+}

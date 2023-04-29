03886041449aa8e1da2e4ae10448da05064149c8
Anh Phuong <trananhphuong1187@gmail.com>
2023-04-29 14:47:20 +0700

    code slider for product images

--- ShopeeClone/src/pages/ProductDetail/ProductDetail.tsx
+++ ShopeeClone/src/pages/ProductDetail/ProductDetail.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import DOMPurify from 'dompurify'
-import React from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
 import InputNumber from 'src/components/InputNumber'
 import ProductRating from 'src/components/ProductRating'
+import { Product } from 'src/types/product.type'
 import { formatCurency, formatShortNumber, rateSale } from 'src/utils/utils'
 
 export default function ProductDetail() {
@@ -13,55 +14,102 @@ export default function ProductDetail() {
     queryKey: ['product', id],
     queryFn: () => productApi.getProduct(id as string)
   })
+  const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
+  const [activeImage, setActiveImange] = useState('')
   const product = productDetailData?.data.data
+  const currentImages = useMemo(
+    () => (product ? product?.images.slice(...currentIndexImages) : []),
+    [product, currentIndexImages]
+  )
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      setActiveImange(product.images[0])
+    }
+  }, [product])
+
+  const chooseActiveImage = (img: string) => {
+    setActiveImange(img)
+  }
+
+  const nextSlide = () => {
+    if (currentIndexImages[1] < (product as Product)?.images.length) {
+      setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+
+  const prevSlide = () => {
+    if (currentIndexImages[0] > 0) {
+      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
+
   if (!product) return null
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
                 <img
-                  src={product.image}
+                  src={activeImage}
                   alt={product.name}
                   className='absolute top-0 left-0 h-full w-full bg-white object-cover'
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute top-1/2 left-0 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth={1.5}
-                    stroke='currentColor'
-                    className='h-5 w-5'
+                {currentIndexImages[0] > 0 && (
+                  <button
+                    className='absolute top-1/2 left-0 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                    onClick={prevSlide}
                   >
-                    <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
-                  </svg>
-                </button>
-                {product.images.slice(0, 5).map((img, index) => {
-                  const isActive = index === 0
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth={1.5}
+                      stroke='currentColor'
+                      className='h-5 w-5'
+                    >
+                      <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+                    </svg>
+                  </button>
+                )}
+                {currentImages.map((img) => {
+                  const isActive = img === activeImage
                   return (
-                    <div className='relative w-full pt-[100%] shadow' key={img}>
-                      <img src={img} alt='' className='absolute top-0 left-0 h-full w-full bg-white object-cover' />
+                    <div
+                      className='relative w-full pt-[100%] shadow'
+                      key={img}
+                      onMouseEnter={() => chooseActiveImage(img)}
+                    >
+                      <img
+                        src={img}
+                        alt={product.name}
+                        className='absolute top-0 left-0 h-full w-full bg-white object-cover'
+                      />
                       {isActive && <div className='absolute inset-0 border-2 border-orange'></div>}
                     </div>
                   )
                 })}
-                <button className='absolute top-1/2 right-0 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth={1.5}
-                    stroke='currentColor'
-                    className='h-5 w-5'
+                {currentIndexImages[1] < product.images.length && (
+                  <button
+                    className='absolute top-1/2 right-0 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                    onClick={nextSlide}
                   >
-                    <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
-                  </svg>
-                </button>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth={1.5}
+                      stroke='currentColor'
+                      className='h-5 w-5'
+                    >
+                      <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+                    </svg>
+                  </button>
+                )}
               </div>
             </div>
             <div className='col-span-7'>
@@ -150,8 +198,8 @@ export default function ProductDetail() {
           </div>
         </div>
       </div>
-      <div className='mt-8 bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='mt-8 bg-white p-4 shadow'>
           <div className='captitalize rounded bg-gray-50 p-4 text-lg text-gray-700'>Mô tả sản phẩm</div>
           <div className='mx-4 mt-12 mb-4 text-sm leading-loose'>
             <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(product.description) }} />

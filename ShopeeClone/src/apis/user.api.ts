b0dabf0dc92616f9c6b76f35ab721dc856a9cde3
Anh Phuong <trananhphuong1187@gmail.com>
2023-05-21 17:04:00 +0700

    declare userApi

---
+++ ShopeeClone/src/apis/user.api.ts
@@ -0,0 +1,26 @@
+import { User } from 'src/types/user.type'
+import { ResponseSuccessApi } from 'src/types/utils.type'
+import http from 'src/utils/http'
+
+interface UpdateProfileType extends Omit<User, '_id' | 'roles' | 'email' | 'createdAt' | 'updatedAt'> {
+  password: string
+  newPassword: string
+}
+
+const userApi = {
+  getUser: () => {
+    return http.get<ResponseSuccessApi<User>>('me')
+  },
+  updateUser: (body: UpdateProfileType) => {
+    return http.put<ResponseSuccessApi<User>>('user', body)
+  },
+  uploadAvatar: (body: FormData) => {
+    http.post<ResponseSuccessApi<string>>('user/upload-avatar', body, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
+}
+
+export default userApi
